83b13d2a91b831291bcd25e87a6ae17715903bc7
hiroshi-fukumizu <hiroshi19890606@gmail.com>
2024-10-06 21:56:57 +0900

    first commit

---
+++ sample.py
@@ -0,0 +1,2 @@
+print('hello world 1')
+print('hello world 2')
---
+++ test03.py
@@ -0,0 +1,7 @@
+print('あなたの名前を教えてください。')
+ 
+your_name = input('>> ')
+ 
+print(your_name)
+
+print('こんにちは！ {} さん！'.format(your_name))
